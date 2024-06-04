3e4d8680eed56e6d2a2bf5daccf7d9cf47d5a186
Krshnaprasad <krshnaprasd997@gmail.com>
2024-06-04 11:13:13 -0800

    UI corrections fixed

--- src/HR.jsx
+++ src/HR.jsx
@@ -37,7 +37,7 @@ const HR = () =>{
       .then(response => response.json())
       .then(data => {
 
-        console.log("================================= attend data =====" + data);
+        // console.log("================================= attend data =====" + data);
         setAttendData(data)
 
       })
