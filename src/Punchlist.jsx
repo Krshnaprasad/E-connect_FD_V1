acee7437677b3ca07f1afdd5118b1330c0a77131
Krshnaprasad <krshnaprasd997@gmail.com>
2024-07-13 14:32:49 +0530

    Admin side finished

--- src/Punchlist.jsx
+++ src/Punchlist.jsx
@@ -7,7 +7,7 @@ const Punchlist = () =>{
 return(
     <>
     <div className="container-fluid project pt-5 pb-5  ">
-    <div className="container">
+    <div className="container pb-5">
         <div className="row text-center">
             <span className="fs-1 fw-bold pb-3">Punchlist</span>
             <Table striped bordered hover>
