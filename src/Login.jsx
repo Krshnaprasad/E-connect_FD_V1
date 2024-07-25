74895cc141ea58306dc0cac843b468e90e219648
Krshnaprasad <krshnaprasd997@gmail.com>
2024-07-25 21:15:34 +0530

    Login  fixed

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import Log from '../src/assets/Login.avif'
 import { useNavigate } from "react-router-dom"
-import FloatingLabel from 'react-bootstrap/FloatingLabel';
+import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 
 // import { useHistory } from 'react-router-dom';
@@ -69,52 +69,37 @@ const Login = () => {
         <>
             <div className="container-fluid pt-5 pb-5" style={{ height: "3em" }}>
                 <div className="container">
-                    <div className="row row-cols-lg-2 row-cols-1">
+                    <div className="row">
                         <div className="col">
                             <img className='img-fluid' src={Log}></img>
                         </div>
                         <div className="col align-content-center">
                             <div className="row justify-content-center">
-                                {/* <form className="card  p-5 " style={{width:"20em"}} onSubmit={handleSubmit}>
-                      
+                                <form className="card logcard  p-5 text-white " style={{width:"20em"}} onSubmit={handleSubmit}>
+                        <h1 className="txt fs-2 fw-bold text-center">E-connect</h1><br></br>
                         <label className="fnt">Name:</label>
                         <input type="text" name="name" placeholder="Name" value={User.value} onChange={handleChange}></input><br></br>
                        
                         <label className="fnt">Password:</label>
-                        <input type="password" name="password" placeholder="Password" value={User.value} onChange={handleChange}></input><br></br>
-                       
+                        <input type="password" name="password" placeholder="Password" value={User.value} onChange={handleChange}></input>
+                        <div className="small text-white-50 text-end mt-2"><span>Forgot password !</span></div><br></br>
                         <div>
-                        <input value={User.value} className="form-check-input" name="check" type="checkbox" role="switch" onChange={handleChange}></input><label className="txt" htmlFor="flexSwitchCheckChecked">Remember me</label><br></br><br></br>
+                        <input value={User.value} className="form-check-input" name="check" type="checkbox" role="switch" onChange={handleChange}></input>&nbsp;&nbsp;<label className="txt" htmlFor="flexSwitchCheckChecked">Remember me</label><br></br><br></br>
                         
                         </div>
                         <button  className="text-white fs-5 border-0 p-1 rounded-2 bg-primary " >Submit</button>
-                        </form> */}
-                                <form className="card logcard  p-5 " style={{ width: "21em" }} onSubmit={handleSubmit}>
-                                    <h1 className=" fs-3 fw-bold text-center text-white">Login</h1><br></br>
-                                    <FloatingLabel
-                                        controlId="floatingInput"
-                                        label="Name"
-                                        className="mb-4"
-                                        value={User.value} onChange={handleChange}
-                                    >
-                                        <Form.Control size='sm' type="email" placeholder="name@example.com" />
-                                    </FloatingLabel>
-                                    <FloatingLabel className='mb-2' controlId="floatingPassword" label="Password" value={User.value} onChange={handleChange}>
-                                        <Form.Control size='sm' type="password" placeholder="Password" />
-                                    </FloatingLabel>
-                                    <span className='text-end small mb-4 text-white-50'>Forgot password</span>
-                                    <div>
-                                        <input value={User.value} className="form-check-input" name="check" type="checkbox" role="switch" onChange={handleChange}></input>&nbsp;&nbsp;<label className="text-white" htmlFor="flexSwitchCheckChecked">Remember me</label><br></br><br></br>
-
+                        </form>
+                               
                                     </div>
-                                    <button className="text-white fs-5 border-0 p-1 rounded-2 bg-primary " >Submit</button>
-                                </form> </div>
+                                   
+                               
+                            </div>
 
                         </div>
 
                     </div>
                 </div>
-            </div>
+            
         </>
     )
 }
